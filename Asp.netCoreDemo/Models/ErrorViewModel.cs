711632a8cad61037b7f17efcb8f793e9be53a08f
rustem <rustemt@gmail.com>
2018-02-27 16:42:16 +0300

    initial
    initial

---
+++ Asp.netCoreDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp.netCoreDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
